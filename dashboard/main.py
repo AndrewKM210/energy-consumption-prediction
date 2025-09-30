460ffa26c18ae5082ef0b00b2d81c6ece6151366
AndrewKM210 <andrewkm210@gmail.com>
2025-09-30 23:29:25 +0200

    Added simple streamlit dashboard app with choropleth map

---
+++ dashboard/main.py
@@ -0,0 +1,48 @@
+import streamlit as st
+import requests
+import pandas as pd
+import plotly.express as px
+import pycountry
+
+
+# Contains the ISO-2 values for all countries
+countries_db = [x.alpha_2 for x in pycountry.countries]
+
+# Set streamlit title
+st.title("EU Energy Consumption Forecasts")
+
+# Year selector
+year = st.slider("Select Year", min_value=2000, max_value=2030, value=2025)
+
+# Get prediction from FastAPI backend
+API_URL = "http://127.0.0.1:8000/predict-year"
+response = requests.get(f"{API_URL}", params={"year": year})
+
+# If reponse is okay, parse response and convert countries from ISO-2 to ISO-3 values
+if response.status_code == 200:
+    data = response.json()
+    df = pd.DataFrame(data)    
+    df = df[df["country"].isin(countries_db)]
+    df["country"] = df["country"].apply(lambda x: pycountry.countries.get(alpha_2=x).alpha_3)
+else:
+    st.error("API request failed")
+    df = pd.DataFrame(columns=["country", "TOE_HAB"])
+
+# Plot choropleth map
+if not df.empty:
+    fig = px.choropleth(
+        df,
+        locations="country",
+        locationmode="ISO-3",
+        color="TOE_HAB",
+        hover_name="country",
+        color_continuous_scale="Viridis",
+        title=f"Predicted Energy Consumption in {year}",
+        scope="europe"
+    )
+    fig.update_layout(
+        autosize=True,
+        margin={"r":0,"t":0,"l":0,"b":0},  # remove empty borders
+    )
+
+    st.plotly_chart(fig, use_container_width=True)
